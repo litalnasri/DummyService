39fa9123e46fd8631b312449a4d693cbcf4e0c8a
LAPTOP-UG4HURCH\Lital <litalnasri1@gmail.com>
2018-01-25 22:22:10 +0200

    delete /

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://custom-activity-poc.herokuapp.com/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://custom-activity-poc.herokuapp.com');
 
     // Request methods you wish to allow
     // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
