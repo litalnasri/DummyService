8e5de4bc0262de8d957908fdc31ac8d64ad92ba5
LAPTOP-UG4HURCH\Lital <litalnasri1@gmail.com>
2018-04-12 12:48:08 +0300

    fix

--- js/dataManager.js
+++ js/dataManager.js
@@ -95,11 +95,11 @@ exports.getData = function(req, res) {
     console.log("isValid");
 
     var key = "1iVc9FDnmSOGH77PYC0iEHQlXfGlvRRsEDGMS3SLB0ce04nOOLSPWa7EtEDhsjfhmXH9tLYeaMMATagRx2I6g8xJJRCCsqseO9HhMj7a8FlJkdxhKfpc6PuELQ81cQJ_Qc3wK9qsCXB95NBaUk6O91wpNHF3-8e0l2-yCaMzanLLl4cSnzFy4cXEYCfDFKmhPdl6WeWq5ySbjOLpFC4klgAVVG-ZJslCDyVvcqpEA4q8fvnOWJ9iEPItTMny5w2";
-    verifyJwt(req.body, key, (err, decoded) => {
+    verifyJwt(req.headers.authorization.split(' '), key, (err, decoded) => {
 		// verification error -> unauthorized request
 		if (err) {
 			console.error(err);
-			return res.status(401).end();
+			// return res.status(401).end();
 		}
 
 		if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
@@ -109,14 +109,15 @@ exports.getData = function(req, res) {
 		
 		} else {
 			console.error('inArguments invalid.');
-			return res.status(400).end();
+			// return res.status(400).end();
 		}
 	});
 
     console.log(req.body);
 
     console.log('sender: ' + req.connection.remoteAddress);
-    console.log('req headers: ' + req.headers['x-forwarded-for']);
+    console.log('req headers x: ' + req.headers['x-forwarded-for']);
+    console.log("req headers: " + JSON.stringify(req.headers));
     console.log("res headers: " + JSON.stringify(res.headers));
     res.sendStatus(200);
   }
