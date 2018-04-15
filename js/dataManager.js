5b1334bba82732b6f44b73069f8aae4b7d3e9959
LAPTOP-UG4HURCH\Lital <litalnasri1@gmail.com>
2018-04-15 13:49:22 +0300

    sd

--- js/dataManager.js
+++ js/dataManager.js
@@ -94,16 +94,17 @@ exports.getData = function(req, res) {
   exports.isValid = function(req, res) {
     console.log("isValid");
 
-    console.log('sender: ' + req.connection.remoteAddress);
-    console.log('req headers x: ' + req.headers['x-forwarded-for']);
-    console.log("req headers: " + JSON.stringify(req.headers));
-    console.log("res headers: " + JSON.stringify(res.headers));
+    // console.log('sender: ' + req.connection.remoteAddress);
+    // console.log('req headers x: ' + req.headers['x-forwarded-for']);
+    // console.log("req headers: " + JSON.stringify(req.headers));
+    // console.log("res headers: " + JSON.stringify(res.headers));
 
+    console.log("body:" + JSON.stringify(req.body));
     var key = "1iVc9FDnmSOGH77PYC0iEHQlXfGlvRRsEDGMS3SLB0ce04nOOLSPWa7EtEDhsjfhmXH9tLYeaMMATagRx2I6g8xJJRCCsqseO9HhMj7a8FlJkdxhKfpc6PuELQ81cQJ_Qc3wK9qsCXB95NBaUk6O91wpNHF3-8e0l2-yCaMzanLLl4cSnzFy4cXEYCfDFKmhPdl6WeWq5ySbjOLpFC4klgAVVG-ZJslCDyVvcqpEA4q8fvnOWJ9iEPItTMny5w2";
     verifyJwt(req.body, (err, decoded) => {
 		// verification error -> unauthorized request
 		if (err) {
-			console.error(err);
+			console.error("error: " + err);
 			// return res.status(401).end();
 		}
 
@@ -118,7 +119,7 @@ exports.getData = function(req, res) {
 		}
 	});
 
-    console.log(req.body);
+    // console.log(req.body);
 
     
     res.sendStatus(200);
