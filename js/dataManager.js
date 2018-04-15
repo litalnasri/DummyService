b6ef38a4b23cb4cc4badd0b04b0b2ffbff08bfc9
LAPTOP-UG4HURCH\Lital <litalnasri1@gmail.com>
2018-04-15 13:40:15 +0300

    sd

--- js/dataManager.js
+++ js/dataManager.js
@@ -100,7 +100,7 @@ exports.getData = function(req, res) {
     console.log("res headers: " + JSON.stringify(res.headers));
 
     var key = "1iVc9FDnmSOGH77PYC0iEHQlXfGlvRRsEDGMS3SLB0ce04nOOLSPWa7EtEDhsjfhmXH9tLYeaMMATagRx2I6g8xJJRCCsqseO9HhMj7a8FlJkdxhKfpc6PuELQ81cQJ_Qc3wK9qsCXB95NBaUk6O91wpNHF3-8e0l2-yCaMzanLLl4cSnzFy4cXEYCfDFKmhPdl6WeWq5ySbjOLpFC4klgAVVG-ZJslCDyVvcqpEA4q8fvnOWJ9iEPItTMny5w2";
-    verifyJwt(req.body, key, (err, decoded) => {
+    verifyJwt(req.body, (err, decoded) => {
 		// verification error -> unauthorized request
 		if (err) {
 			console.error(err);
