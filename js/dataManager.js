0ad1aa56e75f8009d05bdbcbf5a78100520247e5
LAPTOP-UG4HURCH\Lital <litalnasri1@gmail.com>
2018-04-15 14:23:51 +0300

    zdf

--- js/dataManager.js
+++ js/dataManager.js
@@ -109,8 +109,8 @@ exports.getData = function(req, res) {
     console.log("** verify **");
 
     var key = "1iVc9FDnmSOGH77PYC0iEHQlXfGlvRRsEDGMS3SLB0ce04nOOLSPWa7EtEDhsjfhmXH9tLYeaMMATagRx2I6g8xJJRCCsqseO9HhMj7a8FlJkdxhKfpc6PuELQ81cQJ_Qc3wK9qsCXB95NBaUk6O91wpNHF3-8e0l2-yCaMzanLLl4cSnzFy4cXEYCfDFKmhPdl6WeWq5ySbjOLpFC4klgAVVG-ZJslCDyVvcqpEA4q8fvnOWJ9iEPItTMny5w2";
-    console.log("bosy stringify: " + JSON.stringify(req.body));
-    verifyJwt(JSON.stringify(req.body), key, (err, decoded) => {
+    console.log("body stringify: " + req.body.toString('utf8'));
+    verifyJwt(req.body.toString('utf8'), key, (err, decoded) => {
 		// verification error -> unauthorized request
 		if (err) {
 			console.error("error: " + err);
