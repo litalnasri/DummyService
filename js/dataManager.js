5c4c67bf7b78407d356526e12b1071d7441fc935
LAPTOP-UG4HURCH\Lital <litalnasri1@gmail.com>
2018-04-15 13:33:49 +0300

    change jwt

--- js/dataManager.js
+++ js/dataManager.js
@@ -98,9 +98,9 @@ exports.getData = function(req, res) {
     console.log('req headers x: ' + req.headers['x-forwarded-for']);
     console.log("req headers: " + JSON.stringify(req.headers));
     console.log("res headers: " + JSON.stringify(res.headers));
-    
+
     var key = "1iVc9FDnmSOGH77PYC0iEHQlXfGlvRRsEDGMS3SLB0ce04nOOLSPWa7EtEDhsjfhmXH9tLYeaMMATagRx2I6g8xJJRCCsqseO9HhMj7a8FlJkdxhKfpc6PuELQ81cQJ_Qc3wK9qsCXB95NBaUk6O91wpNHF3-8e0l2-yCaMzanLLl4cSnzFy4cXEYCfDFKmhPdl6WeWq5ySbjOLpFC4klgAVVG-ZJslCDyVvcqpEA4q8fvnOWJ9iEPItTMny5w2";
-    verifyJwt(req.headers.authorization.split(' '), key, (err, decoded) => {
+    verifyJwt(req.body, key, (err, decoded) => {
 		// verification error -> unauthorized request
 		if (err) {
 			console.error(err);
@@ -108,7 +108,7 @@ exports.getData = function(req, res) {
 		}
 
 		if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
-			let serviceCloudId;
+		
 
             console.log("decoded: " + JSON.stringify(decoded.inArguments));
 		
